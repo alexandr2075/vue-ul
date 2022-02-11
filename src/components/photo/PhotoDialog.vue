4f3ed8c63e768e7a10ae0704f35d3ab895b13a0c
alex2075 <gagara5620@gmail.com>
2022-02-11 18:44:28 +0600

    add

---
+++ src/components/photo/PhotoDialog.vue
@@ -0,0 +1,46 @@
+<template>
+   <v-dialog
+        v-model="value"
+        max-width="600"
+      >
+        <v-card>
+            <v-card-title>{{photo.title}}</v-card-title>
+            <v-card-text>
+                <v-img
+                 :src="photo.url"
+                />
+            </v-card-text>
+        </v-card>
+      </v-dialog>
+</template>
+
+<script>
+export default {
+    props: {
+        photo: {
+            type: Object,
+            required: true
+        },
+        value: {
+            type: Boolean,
+            default: false
+        }
+    },
+    created() {
+        this.dialogVisible = this.value
+    },
+    data: () => ({
+        dialogVisible: false,
+    }),
+    watch: {
+        value(newValue) {
+            console.log(newValue);
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
